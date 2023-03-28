45cea4eafd42efbb87ec36fa179c41c4756185ae
Daniel Perry <dperry@danieltperry.me>
2023-03-27 21:16:28 -0400

    Use conda run instead

--- ml-interface.sh
+++ ml-interface.sh
@@ -1,14 +1,5 @@
 #! /bin/bash
 
-# This script is a short wrapper around a python script that
-# generates text, intended for OpenMW but probably useful for
-# other applications.
-
-# Set the PYTHONPATH to the directory containing the libraries
-# needed for execution
-# By default, this folder is located alongside the script in a
-# directory called pythonpath
-export PYTHONPATH="$(dirname $0)/pythonpath"
-
-# Call the python script
-python3 "$(dirname $0)/ml-interface.py" "$@"
+#Use conda run to start the interface script
+CONDA_ENV_NAME=openmw_ml
+conda run -n $CONDA_ENV_NAME python3 "$(dirname $0)/ml-interface.py" "$@"
