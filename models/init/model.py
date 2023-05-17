781b360943748671225b1f5dd78df5ac7096017c
Daniel Perry <dperry@danieltperry.me>
2023-05-17 15:23:41 -0400

    Add init model for clean 'ml-interface.sh init' command.

---
+++ models/init/model.py
@@ -0,0 +1,14 @@
+'''
+init model
+Doesn't do anything, just a model to run that
+ensure the whole process works end-to-end.
+'''
+
+# Requirements: None
+
+class Model:
+    def __init__(self):
+        pass
+
+    def predict(self, input_json):
+        return ""
