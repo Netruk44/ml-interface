f1e4c0efc6c5e2fefe1fca7b7d2c487c58c2fdf1
Daniel Perry <dperry@danieltperry.me>
2023-05-17 16:10:33 -0400

    Move 'init' and 'clean' into shell script

--- ml-interface.sh
+++ ml-interface.sh
@@ -4,14 +4,27 @@ VENV_NAME=openmw_ml
 VENV_DIR="$HOME/.venv/$VENV_NAME"
 REQUIREMENTS_FILE="$(dirname "$0")/requirements.txt"
 
-# Check if the venv exists, and create it if it doesn't
-if [ ! -d "$VENV_DIR" ]; then
-    echo "Creating virtual environment..."
+# Check if 'init' or 'clean' was passed as an argument
+if [ "$1" == "init" ] || [ ! -d "$VENV_DIR" ]; then
+    
+    # Check if the venv already exists
+    if [ -d "$VENV_DIR" ]; then
+        echo "ERROR: Virtual environment already exists."
+        exit 1
+    fi
+
+    echo "Initializing virtual environment..."
     python3 -m venv "$VENV_DIR"
     source "$VENV_DIR/bin/activate"
     echo "Installing requirements..."
     pip install -r "$REQUIREMENTS_FILE"
     deactivate
+    echo "Init completed, ready to run models."
+    exit 0
+elif [ "$1" == "clean" ]; then
+    echo "Cleaning virtual environment..."
+    rm -rf "$VENV_DIR"
+    exit 0
 fi
 
 # Activate the venv and start the interface script
